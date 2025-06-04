daadda71a66fafdeec2c9f8bd8e815375e2beade
Z4nR <ari.comunity1@gmail.com>
2025-06-04 07:05:16 +0700

    add authentication file

---
+++ migrations/1748886139450_create-table-authentications.js
@@ -0,0 +1,35 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+const shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+const up = (pgm) => {
+  pgm.createTable('authentications', {
+    token: {
+      type: 'TEXT',
+      notNull: true,
+    },
+  });
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+const down = (pgm) => {
+  pgm.dropTable('authentications', {
+    ifExists: true,
+  });
+};
+
+module.exports = {
+  shorthands,
+  up,
+  down,
+};
